0a5e20eebbfd343507be31945e6b1506b71866a0
Ghislain Gripon <ghislain.gripon@protonmail.com>
2025-03-02 17:01:00 +0100

    Comparison chaining class like in python plus recursion for safety check of report in input

---
+++ src/main/scala/com/ghislaingripon/utils/InputReader.scala
@@ -0,0 +1,21 @@
+package com.ghislaingripon.utils
+
+import scala.io.{BufferedSource, Source}
+
+object InputReader {
+
+  def readInput(filePath: String): List[String] = {
+    val inputStream: BufferedSource = Source.fromFile(filePath)
+    val lines = inputStream.getLines().toList
+    inputStream.close()
+    lines
+  }
+
+  def splitInput(input: String): Array[Int] = {
+    splitInput(input, "\\s+")
+  }
+
+  def splitInput(input: String, splitPattern: String): Array[Int] = {
+    input.split(splitPattern).map(_.toInt)
+  }
+}
